06b23da2943a4deb7cc03aa0a7a522fb2e3ba6f6
Kuba Mracek <mracek@apple.com>
2023-09-19 15:36:16 -0700

    [embedded] Specialize vtables when processing instructions, not bulk

--- include/swift/SILOptimizer/OptimizerBridging.h
+++ include/swift/SILOptimizer/OptimizerBridging.h
@@ -246,7 +246,9 @@ struct BridgedPassContext {
   SWIFT_IMPORT_UNSAFE
   OptionalBridgedValue constantFoldBuiltin(BridgedInstruction builtin) const;
 
-  bool specializeVTables(BridgedFunction function) const;
+  swift::SILVTable *specializeVTableForType(swift::SILType type, BridgedFunction function) const;
+
+  bool specializeClassMethodInst(BridgedInstruction cm) const;
 
   bool specializeAppliesInFunction(BridgedFunction function, bool isMandatory) const;
 
